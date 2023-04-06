4848d40438a4c09a97488dbaf0ef38138a6bc830
juanignacio87 <97361082+juanignacio87@users.noreply.github.com>
2023-04-06 18:38:51 -0300

    Agregar archivos de proyecto.

---
+++ Menu.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Bike_shop
+{
+    /// <summary>
+    /// Lógica de interacción para Menu.xaml
+    /// </summary>
+    public partial class Menu : Page
+    {
+        public Menu()
+        {
+            InitializeComponent();
+        }
+
+        private void mailS_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Contact.xaml", UriKind.Relative));
+        }
+
+        private void liveSupport_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/Discussion.xaml", UriKind.Relative));
+        }
+
+        private void Products_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/ProductsManagmentsMVVM.xaml", UriKind.Relative));
+        }
+    }
+}
